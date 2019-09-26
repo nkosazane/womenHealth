b732ac0a4252aa797e00e0a93fed530b14798e8f
nkosazane <nkosazanembokazi@gmail.com>
2019-09-26 14:21:30 +0200

    reset

--- src/app/page/recover/recover.page.ts
+++ src/app/page/recover/recover.page.ts
@@ -10,47 +10,53 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['./recover.page.scss'],
 })
 export class RecoverPage implements OnInit {
+  isForgotPassword: boolean;
+  emailInput;
+  email: string;
   validations_form: FormGroup;
   errorMessage: string = '';
-  
+  responseMessage: string;
+  responseMessageType: string;
   constructor(
     private navCtrl: NavController,
     private authService: AuthService,
     private formBuilder: FormBuilder,
     private afauth: AngularFireAuth,
   ) { }
-
+  getAuth() { 
+    return this.afauth.auth; 
+  } 
   ngOnInit() {
     
-    this.validations_form = this.formBuilder.group({
-      email: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-      ])),
-      password: new FormControl('', Validators.compose([
-        Validators.minLength(5),
-        Validators.required
-      ])),
-    });
+    // this.validations_form = this.formBuilder.group({
+    //   email: new FormControl('', Validators.compose([
+    //     Validators.required,
+    //     Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    //   ])),
+    //   password: new FormControl('', Validators.compose([
+    //     Validators.minLength(5),
+    //     Validators.required
+    //   ])),
+    // });
   }
  
-  validation_messages = {
-    'email': [
-      { type: 'required', message: 'Email is required.' },
-      { type: 'pattern', message: 'Please enter a valid email.' }
-    ],
-    'password': [
-      { type: 'required', message: 'Password is required.' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long.' }
-    ]
-  };
+  // validation_messages = {
+  //   'email': [
+  //     { type: 'required', message: 'Email is required.' },
+  //     { type: 'pattern', message: 'Please enter a valid email.' }
+  //   ],
+  //   'password': [
+  //     { type: 'required', message: 'Password is required.' },
+  //     { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+  //   ]
+  // };
  
-  recover() {
-    this.authService.recover(this.email)
+  recover(value) {
+    this.authService.recover(value)
       .then(res => {
         console.log(res);
         this.errorMessage = "";
-        // this.navCtrl.navigateBack('/login');
+        this.navCtrl.navigateBack('/login');
       },
       err => {
         console.log(err);
@@ -58,5 +64,46 @@ export class RecoverPage implements OnInit {
         
       })
     }
- 
+    // resetPassword(email: string) {
+    //   this.authService.resetPassword(email)
+    // }
+    // resetPassword() { 
+    //   if (!this.email) { 
+    //     alert('Type in your email first'); 
+    //   }
+    //   this.authService.resetPasswordInit(this.email) 
+    //   .then(
+    //     () => alert('A password reset link has been sent to your email address'), 
+    //     (rejectionReason) => alert(rejectionReason)) 
+    //   .catch(e => alert('An error occurred while attempting to reset your password')); 
+    // }
+    // reset(value){
+    //   this.authService.reset(this.email)
+    //   .then(res => {
+    //     console.log(res);
+    //     this.errorMessage = "";
+    //     this.navCtrl.navigateForward('/login');
+    //   }, err => {
+    //     this.errorMessage = err.message;
+    //     console.log(err);
+    //   })
+    // }
+     // Send link on given email to reset password
+     showMessage(type, msg) {
+      this.responseMessageType = type;
+      this.responseMessage = msg;
+      setTimeout(() => {
+        this.responseMessage = "";
+      }, 2000);
+    }
+    forgotPassword() {
+      this.authService.sendPasswordResetEmail(this.emailInput)
+        .then(res => {
+          console.log(res);
+          this.isForgotPassword = false;
+          this.showMessage("success", "Please Check Your Email");
+        }, err => {
+          this.showMessage("danger", err.message);
+        });
+    }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private afauth: AngularFireAuth,
+  ) { }
   registerUser(value){
     return new Promise<any>((resolve, reject) => {
       firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
@@ -45,8 +48,32 @@ export class AuthService {
        }
      })
    }
-  
+   
+   resetPassword(email: string) {
+    var auth = firebase.auth();
+    return auth.sendPasswordResetEmail(email)
+      .then(() => console.log("email sent"))
+      .catch((error) => console.log(error))
+  }
+
    userDetails(){
      return firebase.auth().currentUser;
    }
+   getAuth() { 
+    return this.afauth.auth; 
+  } 
+  // resetPasswordInit(email: string) { 
+  //   return this.afauth.auth.sendPasswordResetEmail(
+  //     email, 
+  //     { url: 'http://localhost:4200/auth' }); 
+  //   } 
+    // reset(email : string){
+    //   return new Promise<any>((resolve, reject) => {
+    //   firebase.auth().sendPasswordResetEmail(email)
+    //   .then((res: any) => console.log(res))
+    //   .catch((error: any) => console.error(error));
+    //   })}
+    async sendPasswordResetEmail(passwordResetEmail: string) {
+      return await this.afauth.auth.sendPasswordResetEmail(passwordResetEmail);
+    }
 }
